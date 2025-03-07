98cd2197414698bfced5214b167875e695b7c3d6
João de Sousa <joao@pedrodesousa.pt>
2025-03-07 19:02:29 +0000

    Added UMAMI

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt" suppressHydrationWarning>
+      <head>
+      <script defer src="https://umami.outplay.cloud/script.js" data-website-id="fc1a5619-75ce-4100-9357-7baeac458ce7"></script>
+      </head>
       <body className={cn("min-h-screen bg-background font-sans antialiased")}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <div className="relative flex min-h-screen flex-col">
